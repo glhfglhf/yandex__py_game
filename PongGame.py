e87f3baa1a14781ede4ef27d2970260b7e731a2c
StarScreamsss <90149929+StarScreamsss@users.noreply.github.com>
2022-01-15 18:38:49 +0300

    final commit

--- PongGame.py
+++ PongGame.py
@@ -13,10 +13,12 @@ class Window(QWidget):
         uic.loadUi('Pong_choose_menu.ui', self)
         self.operation = str()
         self.init_ui()
+        self.window = Window
         self.setFixedSize(self.size())
         global col_num_1
         global col_num_2
 
+    # initialize
     def init_ui(self):
         self.pushButton_9.clicked.connect(self.start)
         self.pushButton_5.clicked.connect(self.start)
@@ -26,22 +28,33 @@ class Window(QWidget):
         self.pushButton_2.clicked.connect(self.start)
         self.pushButton_3.clicked.connect(self.start)
         self.pushButton_4.clicked.connect(self.start)
+        self.horizontalSlider.valueChanged.connect(self.sound)
 
+    # sound menu settings
+    def sound(self):
+        global sound_value
+        value = str(self.horizontalSlider.value())
+        sound_value = int(value)
+        self.label_14.setText(value)
+
+    # start game
     def start(self):
         send = self.sender().text()
         if send != '1' and send != '2' and send != '3' and send != '4':
             self.hide()
             pong(send)
-            self.label_8.setText(str(col_num_1))
-            self.label_9.setText(str(col_num_2))
+            self.show()
+            self.label_8.setText(str(col_num_2))
+            self.label_9.setText(str(col_num_1))
+            # results
             if col_num_1 > col_num_2:
-                self.label_10.setText('Игрок № 1 победил!')
+                self.label_10.setText('Игрок № 2 победил!')
             elif col_num_1 == col_num_2:
                 self.label_10.setText('Ничья')
             else:
-                self.label_10.setText('Игрок № 2 победил!')
-            self.show()
+                self.label_10.setText('Игрок № 1 победил!')
         else:
+            # fon settings
             global fon_number
             if send == '1':
                 fon_number = '1'
@@ -54,8 +67,18 @@ class Window(QWidget):
             self.label_13.setText('Фон успешно выбран!')
 
 
+# global game settings
+sound_value = 0
+col_num_1 = 0
+col_num_2 = 0
+fon_number = ''
+
+
+# game
 def pong(send):
     global level
+    global sound_value
+    # game mode settings
 
     def bot1lvl():
         if paddle.x + 75 > ball.x:
@@ -96,6 +119,7 @@ def pong(send):
         if key[pygame.K_RIGHT] and paddle.right < width:
             paddle.right += paddle_speed
 
+    # paddle collision
     def detect_collision(d_x, d_y, boll, rect):
         if d_x > 0:
             delta_x = boll.right - rect.left
@@ -114,6 +138,8 @@ def pong(send):
             d_x = -d_x
         sound_collision.play()
         return d_x, d_y
+    sound_value = sound_value / 100
+    # set mode
     if send == '1 на 1':
         level = 11
     elif send == "Бот: уровень легко":
@@ -135,6 +161,7 @@ def pong(send):
     ball_speed = 6
     ball_rect = int(ball_radius * 2 ** 0.5)
     ball = pygame.Rect(rnd(ball_rect, width - ball_rect), height // 2, ball_rect, ball_rect)
+    # ball direction
     a = random.randint(0, 1)
     if a == 1:
         dx = 1
@@ -148,6 +175,7 @@ def pong(send):
     pygame.init()
     sc = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
+    # stats
     col1 = 0
     col2 = 0
     # background image
@@ -165,7 +193,7 @@ def pong(send):
     else:
         img = pygame.image.load('fon.jpg').convert()
     start_ticks = pygame.time.get_ticks()  # starter tick
-    # sound
+    # sound and music
     sound_collision = pygame.mixer.Sound("collision.wav")
     sound_loose = pygame.mixer.Sound("loose.wav")
     sound_win = pygame.mixer.Sound("win.mp3")
@@ -173,18 +201,28 @@ def pong(send):
     sound_collision_wall = pygame.mixer.Sound("collision_wall.wav")
     start_sound = pygame.mixer.Sound('start.wav')
     pygame.mixer.music.load("fon.mp3")
+    sound_win.set_volume(sound_value)
+    start_sound.set_volume(sound_value)
+    sound_collision_wall.set_volume(sound_value)
+    sound_collision.set_volume(sound_value)
+    sound_loose.set_volume(sound_value)
+    sound_not_win.set_volume(sound_value)
     running = True
     pygame.mixer.music.play(-1)
+    # application name
     pygame.display.set_caption('Pong')
+    pygame.mixer.music.set_volume(sound_value)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                pygame.display.quit()
                 pygame.mixer.music.stop()
+                return
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    running = False
+                    pygame.display.quit()
                     pygame.mixer.music.stop()
+                    return
         seconds = (pygame.time.get_ticks() - start_ticks) / 1000  # calculate how many seconds
         if seconds > 4:
             # ball movement
@@ -214,6 +252,7 @@ def pong(send):
                     dy = 1
                 else:
                     dy = -1
+            # loose player 2
             if ball.centery < 40:
                 a = random.randint(0, 1)
                 dx, _ = 1, -1
@@ -231,7 +270,7 @@ def pong(send):
                 fps = 60
                 col1 += 1
                 sound_loose.play()
-
+            # loose player 1
             if ball.centery > 760:
                 ball.x = random.randint(50, 750)
                 ball.y = 400
@@ -268,13 +307,19 @@ def pong(send):
                 paddle1.left -= paddle_speed
             if key[pygame.K_d] and paddle1.right < width:
                 paddle1.right += paddle_speed
+        global col_num_1
+        global col_num_2
+        col_num_1 = col1
+        col_num_2 = col2
+        # win or loose sound and exit
         if (level == 1 or level == 2 or level == 3) and col2 >= 10:
             sound_win.play()
         elif (level == 1 or level == 2 or level == 3) and col1 >= 10:
             sound_not_win.play()
         if col1 >= 10 or col2 >= 10:
-            running = False
+            pygame.display.quit()
             pygame.mixer.music.stop()
+            return
         sc.blit(img, (0, 0))
         # drawing world
         pygame.draw.rect(sc, pygame.Color('Orange'), (0, 400, 800, 15), 2)
@@ -319,6 +364,7 @@ def pong(send):
         text_rect_obj = text_surface_obj.get_rect()
         text_rect_obj.center = (50, 450)
         sc.blit(text_surface_obj, text_rect_obj)
+        # game mode settings view
         if level == 1:
             font = pygame.font.Font('freesansbold.ttf', 15)
             text_surface_obj = font.render(f'Mode: bot vs people level: easy', True, pygame.Color('red'))
@@ -345,15 +391,9 @@ def pong(send):
             sc.blit(text_surface_obj, text_rect_obj)
         pygame.display.flip()
         clock.tick(fps)
-        global col_num_1
-        global col_num_2
-        col_num_1 = col1
-        col_num_2 = col2
 
 
-col_num_1 = 0
-col_num_2 = 0
-fon_number = ''
+# window start
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     exe = Window()
